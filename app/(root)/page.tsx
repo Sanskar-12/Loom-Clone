a6269098dc821d3e013b471513ece53d6450e518
Sanskar-12 <sanskarv2004@gmail.com>
2025-06-22 16:46:56 +0530

    header completed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react' 
+import Header from "@/components/Header";
+import React from "react";
 
 const Page = () => {
   return (
-    <div className='text-2xl font-karla'>Welcome to Loom</div>
-  )
-}
+    <main className="wrapper page">
+      <Header title="All Videos" subHeader="Public Library" />
+      Welcome to Loom
+    </main>
+  );
+};
 
-export default Page
+export default Page;
